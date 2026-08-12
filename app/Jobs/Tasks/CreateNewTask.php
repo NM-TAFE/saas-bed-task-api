28e09e1e456f39f9cbddcefac3b14f64a26e7fd1
John Robertson <ROBERJ@markonetechnologies.com.au>
2026-08-12 12:56:24 +0800

    feat: completion of Job and payload use

---
+++ app/Jobs/Tasks/CreateNewTask.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Jobs\Tasks;
+
+use App\Http\Payloads\NewTask;
+use App\Models\Task;
+use Illuminate\Database\DatabaseManager;
+use Throwable;
+
+final readonly class CreateNewTask
+{
+    public function __construct(public NewTask $payload) {}
+
+    /** @throws Throwable */
+    public function handle(DatabaseManager $database): Task
+    {
+        return $database->transaction(
+            callback: fn(): Task => Task::query()->create($this->payload->toArray()),
+            attempts: 3,
+        );
+    }
+}
