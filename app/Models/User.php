0858e10a2d69a4777d44ac57ab80dc4279a3071d
John Robertson <ROBERJ@markonetechnologies.com.au>
2026-08-26 08:09:27 +0800

    feat: update User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,8 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+// use Illuminate\Foundation\Auth\User as Authenticatable;
+use MongoDB\Laravel\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
