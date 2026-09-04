a9130c081063cdbeb9a3cbfc38a901d7949fe94c
John Robertson <john@myjamjar.com.au>
2026-09-04 10:52:43 +0800

    chore: add proper resource type returns to the controllers

--- app/Http/Api/Controllers/Attachments/ShowController.php
+++ app/Http/Api/Controllers/Attachments/ShowController.php
@@ -6,11 +6,10 @@ namespace App\Http\Api\Controllers\Attachments;
 
 use App\Http\Api\Resources\AttachmentResource;
 use App\Models\Attachment;
-use Illuminate\Http\Resources\Json\JsonResource;
 
 final readonly class ShowController
 {
-    public function __invoke(Attachment $attachment): JsonResource
+    public function __invoke(Attachment $attachment): AttachmentResource
     {
         $attachment->load(['attachmentable', 'uploadedBy']);
 
--- app/Http/Api/Controllers/Tasks/ShowController.php
+++ app/Http/Api/Controllers/Tasks/ShowController.php
@@ -6,11 +6,10 @@ namespace App\Http\Api\Controllers\Tasks;
 
 use App\Http\Api\Resources\TaskResource;
 use App\Models\Task;
-use Illuminate\Http\Resources\Json\JsonResource;
 
 final readonly class ShowController
 {
-    public function __invoke(Task $task): JsonResource
+    public function __invoke(Task $task): TaskResource
     {
         $task->load(['user']);
 
